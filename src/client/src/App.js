f34ecb2aa46b8c6fb5f469d7039e7d6983a32170
Giorgio <giorgio.diprima@edu.turkuamk.fi>
2021-07-24 10:58:42 +0300

    Removed last update section

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     return (
       <div className="App">
         < HeaderBar />
-        < LastUpdate />
+        {/* < LastUpdate /> */}
         < TotalDoses />
         < BarCharProducer />
         < Expired />
